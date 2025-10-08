5f2939c1a2cac503f5f50aa3ae444b2620478394
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-10-07 22:28:23 -0400

    Multi version support.

---
+++ src/BetaConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageSort_Bootstrap
+{
+    public class BetaConfig
+    {
+        public string BetaVersion { get; set; }
+        public bool DisableStable { get; set; }
+        public bool DisableBeta { get; set; }
+    }
+}

2cf6193defb7588575ea60fa47d9c0eb25aa1989
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-12-08 15:53:01 -0500

    wip

---
+++ src/Patches/CorpseDrop/CorpseInspectWindowComponent.cs
@@ -0,0 +1,45 @@
+﻿using MGSC;
+using StorageSort.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+
+namespace StorageSort.Patches.BackpackSort
+{
+    /// <summary>
+    /// Handles dropping all items from a corpse.  The component is required as
+    /// CorpseInspectWindow dowes not have an Update function.
+    /// </summary>
+    internal class CorpseInspectWindowComponent : UpdateComponent<CorpseInspectWindow>
+    {
+        public override void Update()
+        {
+            try
+            {
+                if (!Input.GetKey(Plugin.Config.DropKey)) return;
+
+                DropAllItems();
+
+            }
+            catch (Exception ex)
+            {
+
+                Plugin.Logger.LogError(ex);
+            }
+
+        }
+
+        private void DropAllItems()
+        {
+            UI.Get<InventoryScreen>().TakeAllFromCorpse();
+
+            //COPY WARNING: InventoryScreen.TakeAllFromCorpse().
+            //  This is a copy of the drop logic from that function, minus the inventory add logic.
+
+        }
+    }
+}
